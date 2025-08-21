fc098e9e3bcaec199b62a64f158ac01080c6593d
Worawut124 <jayyo5656@gmail.com>
2025-08-21 15:15:26 +0700

    Your commit message

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import "react-day-picker/style.css"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
